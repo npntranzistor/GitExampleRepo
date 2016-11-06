3cc96211b75208b861dfa9734847f5ebb8785749
Tsishuk Sasha <some_smile@mail.ru>
2016-11-06 18:18:26 +0300

    Создана ThirdActivity

--- app/src/main/java/com/example/trygitapplication/SecondActivity.java
+++ app/src/main/java/com/example/trygitapplication/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.example.trygitapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void ButtonThirdClick(View view) {
+        Intent myIntent = new Intent(this, ThirdActivity.class);
+        startActivity(myIntent);
     }
 }
